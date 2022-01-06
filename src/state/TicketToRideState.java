1dd8fb790553dd479cb6a8a5973530660e17ce6a
Aaron Tetens <tetena573@gmail.com>
2022-01-06 00:14:25 -0500

    added mcts project to build path

---
+++ src/state/TicketToRideState.java
@@ -0,0 +1,33 @@
+package state;
+
+import java.util.List;
+
+import api.GameState;
+
+public class TicketToRideState implements GameState {
+
+	@Override
+	public int getLastPlayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<GameState> getNextStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GameState getRandomNextState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> getWinningPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
