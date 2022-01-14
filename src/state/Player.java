86af046af621c3d641ad3d1063f7db59bbb88b9d
Aaron Tetens <tetena573@gmail.com>
2022-01-13 21:46:55 -0500

    Player class skeleton

--- src/state/Player.java
+++ src/state/Player.java
@@ -1,5 +1,23 @@
 package state;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import data.DestinationTicket;
+
 public class Player {
 
+	private final Map<String, Integer> colorCards;
+	private final Set<DestinationTicket> destinationTickets;
+	private int numCarsRemaining;
+	private int score;
+
+	public Player(final int numStartingCars) {
+		this.colorCards = new HashMap<>();
+		this.destinationTickets = new HashSet<>();
+		this.numCarsRemaining = numStartingCars;
+		this.score = 0;
+	}
 }
