a0dcce1065d695a6a3d3caee3df83a875d498ed7
Aaron Tetens <tetena573@gmail.com>
2022-01-27 04:17:38 -0500

    increased timeout

--- src/driver/GameDriver.java
+++ src/driver/GameDriver.java
@@ -32,7 +32,7 @@ public class GameDriver {
 
 		// AI needs to figure out which destination tickets to keep
 		System.out.println("AI is thinking...");
-		gameState = (TicketToRideState) MCTS.search(gameState, 30, 1);
+		gameState = (TicketToRideState) MCTS.search(gameState, 30, 2);
 		gameState.printPlayerInfo(aiPlayer);
 		gameState.getNumDestinationTicketsForHumanPlayers(aiPlayer, in);
 
@@ -42,7 +42,7 @@ public class GameDriver {
 			if (gameState.getCurrentPlayer() == aiPlayer) {
 				gameState.replenishFaceUp(in);
 				System.out.println("AI is thinking...");
-				gameState = (TicketToRideState) MCTS.search(gameState, 30, 1);
+				gameState = (TicketToRideState) MCTS.search(gameState, 30, 2);
 				gameState.resolveUnknownsForPlayerManually(aiPlayer, in);
 				gameState.printPlayerInfo(aiPlayer);
 
